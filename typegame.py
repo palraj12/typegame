cfb726559860af441fefc1739824a9eab6f430a0
Rahul Kumar Jha <67184791+RahulJha393@users.noreply.github.com>
2021-10-24 01:57:54 +0530

    added key controls
    Keyboard strick are in action now, stimulation process improved

--- typegame.py
+++ typegame.py
@@ -36,5 +36,18 @@ while(stop!=0):
         if(tim>=time[0]+1 ):
             print("time up")
             break
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
 print("your score is ",score)
     
